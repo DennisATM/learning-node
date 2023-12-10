ccc247e4efd5cdbf1b91ff26fc8ab04921cb98da
Dennis.Torres <86396553+DennisATM@users.noreply.github.com>
2023-12-09 23:04:19 -0300

    Second commit

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+const { uuidv4, getAge} = require('./plugins');
 
 // const {emailTemplate}=require('./js-foundation/template');
 
@@ -20,5 +21,15 @@
 
 
 // // console.log(emailTemplate);
-require('./js-foundation/factory');
+const {buildMakePerson} = require('./js-foundation/factory');
 
+const makePerson = buildMakePerson( {uuidv4, getAge});
+
+const obj={
+    name:'jhon',
+    birthdate:'1979-10-27'
+};
+
+const jhon=makePerson(obj);
+
+console.log(jhon);
--- src/js-foundation/factory.js
+++ src/js-foundation/factory.js
@@ -1,19 +1,25 @@
-const {uuidv4,getAge}=require('../plugins');
 
-const buildPerson=({name,birthdate})=>{
-    return {
-        id: uuidv4(),
-        name:name,
-        birthdate:getAge(birthdate),
-        age: new Date().getFullYear() - new Date(birthdate).getFullYear(),
+
+const buildMakePerson=({ uuidv4, getAge})=>{
+    return ({ name, birthdate })=>{
+        return {
+            id: uuidv4(),
+            name:name,
+            birthdate:birthdate,
+            age: getAge(birthdate),
+        }
     }
 }
 
-const obj={
-    name:"Jhon", 
-    birthdate:"1979-10-27"
+// const obj={
+//     name:"Jhon", 
+//     birthdate:"1979-10-27"
+// }
+
+// const jhon = buildPerson(obj);
+
+// console.log(jhon);
+
+module.exports={
+ buildMakePerson,   
 }
-
-const jhon = buildPerson(obj);
-
-console.log(jhon);
